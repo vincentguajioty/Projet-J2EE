5718552892ddb7642c0c31f7b728b30d5337c55a
Henri DAVANTURE <h.youyi@hotmail.fr>
2015-05-09 17:37:41 +0200

    SMTP OK Envoi de mail pour recuperation OK

--- Developpement/J2EPro/src/servlets/Recupmdp.java
+++ Developpement/J2EPro/src/servlets/Recupmdp.java
@@ -8,13 +8,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.util.Properties;
-
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import java.util.*;
+import java.io.*;
+import java.nio.file.*;
+import javax.mail.*;
+import javax.mail.internet.*;
 
 import entities.Client;
 import entities.ClientHome;
@@ -24,6 +22,7 @@ public class Recupmdp extends HttpServlet{
 	public static final String CHAMP_EMAIL     = "email";
     public static final String ATT_MESSAGE     = "message2";
     public static final String ATT_ERREUR      = "erreur";
+
 	
 	 public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
 	        /*
@@ -38,6 +37,7 @@ public class Recupmdp extends HttpServlet{
 	        boolean erreur = false;
 	        
 	        Client c = null;
+	        //si lma 
 	        if ( email.trim().isEmpty() ) {
 	            erreur = true;
 	            message2 = "Erreur- Vous n'avez pas rempli tous les champs obligatoires. <br> <a href=\"recupmdp\">Essayer encore</a> ";
@@ -46,48 +46,47 @@ public class Recupmdp extends HttpServlet{
 	        else
 	        {
 	        	c = (new ProxyHome<Client>(new ClientHome())).findByEmail(email);
-	        	if(c == null)
-					throw new NullPointerException("Mail inconnus " + email + " " );
-	        	mailcli = c.getMailCli();
-	        	mdp = c.getMdpCli();
-	        	if (email.contentEquals(mailcli))
-	        	{
-	        		
-	    		    String emailFrom = "\"moi Himself\"<contact@davanture.fr>";
-	    		    String emailTo = email;		
-	    		    String messageText = "Voici votre mot de passe, veuillez en prendre soin. " + " " + mdp;
-	    		    String subject = "Noreply, récupération du mot de passe";
-	    		    
-	    		    try 
-	    		    {   		
-	    		    	Properties props = System.getProperties();
-	    		    	// laisser "mail.smtp.host" en dur
-	    		    	props.put("mail.smtp.host", "ssl0.ovh.net");
-	    		    	// javax.mail.Session pour que ça fonctionne aussi avec WebObjects
-	    		    	javax.mail.Session maSession = javax.mail.Session.getDefaultInstance(props, null);
-	    		    	MimeMessage message = new MimeMessage(maSession);
-	    		 
-	    		    	message.setFrom(new InternetAddress(emailFrom));
-	    		    	message.addRecipient(Message.RecipientType.TO, new InternetAddress(emailTo));
-	    		    	//message.addRecipient(Message.RecipientType.CC, new InternetAddress(emailCc));
-	    		    	message.setSubject(subject);
-	    		    	message.setText(messageText);
-	    		 
-	    			Transport.send(message);    		   		
-	    		    } 
-	    		    catch (AddressException e) {
-	    		    	e.printStackTrace();
-	    		    } 
-	    		    catch (MessagingException e) {
-	    		    	e.printStackTrace();
-	    		    }
-	    		    message2 = "Mail envoyé avec succès";
-	        		
+	        	if(c == null){
+	        		 erreur = true;
+	        		 message2="Il n'y a aucun client enregistré pour cette adresse";
 	        	}
-	        	else
-	        	{
-	        		message2 = "Il n'y a aucun client enregistré pour cette adresse";
-	        		erreur = true;
+	        	else{
+		        	mailcli = c.getMailCli();
+		        	mdp = c.getMdpCli();
+		        	    final String username = "contact@davanture.fr";
+		        		final String password = "456456456a";	// Le premier qui s'amuse à utiliser ce mot de passe à des fins non respectables aura à faire à moi
+		    		    	Properties props = System.getProperties();
+		    		    	// laisser "mail.smtp.host" en dur
+		    		    	props.put("mail.smtp.host", "ssl0.ovh.net");
+		    				props.put("mail.smtp.auth", "true");
+		    				props.put("mail.smtp.starttls.enable", "true");
+		    				props.put("mail.smtp.port", "587");
+		    		    	// javax.mail.Session pour que ça fonctionne aussi avec WebObjects
+		    				Session session = Session.getInstance(props,
+		    						  new javax.mail.Authenticator() {
+		    							protected PasswordAuthentication getPasswordAuthentication() {
+		    								return new PasswordAuthentication(username, password);
+		    							}
+		    				});
+		    				try {
+		    					 
+		    					Message message = new MimeMessage(session);
+		    					message.setFrom(new InternetAddress("contact@davanture.fr"));
+		    					message.setRecipients(Message.RecipientType.TO,
+		    						InternetAddress.parse(email));
+		    					message.setSubject("Noreply Password recovery");
+		    					message.setText("Here's your password : " + " <b> " + mdp +"</b>");
+		    		 
+		    					Transport.send(message);
+		    		 
+		    					System.out.println("Done");
+		    					message2 = "Mail envoyé avec succès, veuillez attendre son envoi.";
+		    				} catch (MessagingException e) {
+		    					throw new RuntimeException(e);
+		    				}
+	  
+	    		   
+	        		
 	        	}
 
 	        }
